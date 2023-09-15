f3d68aff484ea21641f27d5d129f822814c168db
lazyfox16 <ksanka1694@gmail.com>
2023-09-15 15:00:49 +0300

    fix custom exception

--- src/main/java/ru/lazyfox16/mycalc/exceptions/UnrecognizedTokenException.java
+++ src/main/java/ru/lazyfox16/mycalc/exceptions/UnrecognizedTokenException.java
@@ -3,5 +3,6 @@ package ru.lazyfox16.mycalc.exceptions;
 public class UnrecognizedTokenException extends RuntimeException {
 
     public UnrecognizedTokenException(String message) {
+        super(message);
     }
 }
